3932c8493d8dcff3a1ce379a372b53979b0469ca
Enders <raphvdm@gmail.com>
2017-12-01 16:32:23 +0100

    prettier map

--- main.hs
+++ main.hs
@@ -27,13 +27,13 @@ infiniteGenerators :: (RandomGen g) => g -> [g]
 infiniteGenerators = unfoldr (Just . split)
 
 infiniteRandomLists :: (RandomGen g) => g -> [[Int]]
-infiniteRandomLists = map (randomRs (0,100)) . infiniteGenerators
+infiniteRandomLists = map (randomRs (0,99)) . infiniteGenerators
 
-main :: IO b
+main :: IO ()
 main = do
   let ppos = (0,0)
   --params <- paramsLoop
-  let params = Params 1 7 1 50 10 5 10 10
+  let params = Params 1 7 1 10 30 20 0 0
   let tileList = initTileList params
   let randomMat = infiniteRandomLists (mkStdGen (initialSeed params))
   let randomTiles = (map . map) (corresp tileList params) randomMat
@@ -54,6 +54,7 @@ corresp' params proba
   | proba <= treasurelh params = "T"
   | otherwise = "D"
 
+compareTreasure :: String -> String -> String
 compareTreasure x y  =
   if x == "D"
     then
@@ -89,13 +90,25 @@ makeSubMatrix desert startRow endRow startCol endCol
 printMatrix' :: [[String]] -> Int -> Int -> Int -> Int -> PlayerPos -> IO()
 printMatrix' desert startRow endRow startCol endCol ppos = do
   let d = replaceAt ppos desert "Pl"
-  mapM_ print (makeSubMatrix' d startRow endRow startCol endCol)
+  if startRow < 0
+    then
+      if startCol < 0
+        then mapM_ printDesertLine (makeSubMatrix' d (max 0 startRow) (endRow + abs startRow) (max 0 startCol) (endCol + abs startCol))
+        else mapM_ printDesertLine (makeSubMatrix' d (max 0 startRow) (endRow + abs startRow) (max 0 startCol) endCol)
+    else
+      if startCol < 0
+        then mapM_ printDesertLine (makeSubMatrix' d (max 0 startRow) endRow (max 0 startCol) (endCol + abs startCol))
+        else mapM_ printDesertLine (makeSubMatrix' d (max 0 startRow) endRow (max 0 startCol) endCol)
   return()
 
 makeSubMatrix' :: [[String]] -> Int -> Int -> Int -> Int -> [[String]]
 makeSubMatrix' desert startRow endRow startCol endCol
-  | startRow /= endRow = take (endCol - startCol) (drop startCol (desert !! startRow) ) : makeSubMatrix' desert (startRow + 1) endRow startCol endCol
   | startRow == endRow = []
+  | startRow /= endRow = take (endCol - startCol) (drop startCol (desert !! startRow) ) : makeSubMatrix' desert (startRow + 1) endRow startCol endCol
+
+printDesertLine :: [String] -> IO()
+printDesertLine l= print (unwords l)
+
 
 replaceAt :: PlayerPos -> [[String]] -> String -> [[String]]
 replaceAt ppos desert val =
@@ -140,14 +153,15 @@ paramsLoop = do
 
 
 
-gameLoop :: PlayerPos -> Sdesert -> Params -> Int -> Int -> IO b
+gameLoop :: PlayerPos -> Sdesert -> Params -> Int -> Int -> IO ()
 gameLoop ppos desert params currentWater currentTreasures = do
-  printMatrix' desert 0 10 0 10 ppos
+  printMatrix' desert (fst ppos - 5) (fst ppos + 6) (snd ppos - 5) (snd ppos + 6) ppos
   putStrLn "w,a,s,d : "
   input <- getLine
   newpos <- doMove input ppos
   draw newpos
   currentWater' <- fillOrDecrementWater currentWater desert newpos (maxWater params)
+  endGame <- checkEndGame desert (snd newpos) currentWater'
   currentTreasures' <- checkTreasureFound currentTreasures (desert !! fst(snd newpos) !! snd (snd newpos))
   print currentWater'
   print currentTreasures'
@@ -155,11 +169,33 @@ gameLoop ppos desert params currentWater currentTreasures = do
     then do
       desert' <- pickUpTreasure desert (snd newpos)
       _ <- return desert'
-      gameLoop (snd newpos) desert' params currentWater' currentTreasures'
+      if endGame == 0
+        then gameLoop (snd newpos) desert' params currentWater' currentTreasures'
+        else
+          if endGame == 1
+            then putStrLn "You're DEAD !"
+            else
+              when  (endGame == 2) $
+                putStrLn "You WON !"
+
+
     else do
       desert' <- returnDesert desert
       _ <- return desert'
-      gameLoop (snd newpos) desert' params currentWater' currentTreasures'
+      if endGame == 0
+        then gameLoop (snd newpos) desert' params currentWater' currentTreasures'
+        else
+          if endGame == 1
+            then putStrLn "You're DEAD !"
+            else
+              when  (endGame == 2) $
+                putStrLn "You WON !"
+
+getLos :: PlayerPos -> Int -> [PlayerPos]
+getLos ppos los =
+  let xs = [-los..los]
+    in [(x + fst ppos, y + snd ppos) | x <- xs, y <- xs, abs x + abs y <= los]
+
 
 
 doMove :: String -> PlayerPos -> IO(Bool, PlayerPos)
@@ -176,6 +212,12 @@ fillOrDecrementWater currentWater desert newpos maxWater'
 checkTreasureFound :: Int -> String -> IO Int
 checkTreasureFound currentTreasures tile =
   if tile == "T" then return (currentTreasures + 1) else return currentTreasures
+
+checkEndGame :: [[String]] ->  PlayerPos -> Int -> IO Int
+checkEndGame desert ppos currWater
+  | desert !! fst ppos !! snd ppos `elem` ["L", "L'"] || currWater == 0 = return 1
+  | desert !! fst ppos !! snd ppos == "P" = return 2
+  | otherwise = return 0
 
 pickUpTreasure :: [[String]] -> PlayerPos -> IO [[String]]
 pickUpTreasure desert ppos = do
